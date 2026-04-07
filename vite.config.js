d0e99728087b3954b571f9e0b95d354aa39a77d2
Muhamad Rizal <mhmddrizal2u@gmail.com>
2026-04-07 07:00:14 +0700

    Fix: switch to esbuild minifier

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Pakai esbuild biar nggak error baca karakter '%' di SVG noise
+    // Ini kuncinya! Pakai esbuild biar si Netlify nggak rewel
     minify: 'esbuild'
   }
 })
