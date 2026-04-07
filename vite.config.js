38a0849e554c7cdfa2b0cba6eb4a9cfb03ba869f
Muhamad Rizal <mhmddrizal2u@gmail.com>
2026-04-07 07:11:59 +0700

    fix coding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Pakai esbuild biar dia gak rewel soal error SVG Noise
     minify: 'esbuild'
   }
 })
